a2d4116be71982ece620eb73184b053e1e6d7383
DeividsonGonini <deividsongonini@gmail.com>
2022-05-11 01:20:41 -0300

    Adicionados streams com operacoes finais

--- src/main/java/streams/finais/StreamCount.java
+++ src/main/java/streams/finais/StreamCount.java
@@ -7,6 +7,9 @@ public class StreamCount {
     public static void main(String[] args) {
         List<Integer> lista = Arrays.asList(1,5,8,9,1,4,7,6,6,9,9);
 
+        //Lista Original
+        System.out.println("\nLista original" + lista);
+
         //Implementação Nova JAVA 8 Stream Count
         long count = lista.stream()
                 .limit(12)
@@ -14,9 +17,6 @@ public class StreamCount {
                 .count();//Conta quantos elementos passaram pelo count, Ex. caso tenha um filter antes ira contar os que restaram após o filtro
 
         System.out.println("\nContagem de elementos que passaram na lista: " + count);
-
-        //Lista Original
-        System.out.println("\nLista original" + lista);
 
     }
 }
